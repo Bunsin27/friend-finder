805799f4de1da49e12abae96443f431310b6cd2b
stephen England <stephenengland@stephens-MacBook-Pro.local>
2019-12-25 22:06:46 -0500

    changes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,8 +3,8 @@ let path = require('path');
 //ROUTING
 
 module.exports = function(app){
-  //default GET route that leads to home.html - displays home page
-  app.get('/survey', function (req, res) {
+  //default GET route ---- displays home page
+  app.get('./survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
